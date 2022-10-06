03a7522440b25abf33989947f56c6bd456336a6e
LeHuuVu <89371606+LeHuuVu@users.noreply.github.com>
2022-10-06 09:09:02 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,20 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Schema::disableForeignKeyConstraints()
+        Schema::disableForeignKeyConstraints();
         
-        //DB::statement("SET foreign_key_checks=0");
-//         $databaseName = DB::getDatabaseName();
-//         $tables = DB::select("SELECT * FROM information_schema.tables WHERE table_schema = '$databaseName'");
-//         foreach ($tables as $table) {
-//             $name = $table->TABLE_NAME;
-//             //if you don't want to truncate migrations
-//             if ($name == 'migrations') {
-//                 continue;
-//             }
-//             DB::table($name)->truncate();
-//         }
-        //DB::statement("SET foreign_key_checks=1");
+        DB::statement("SET foreign_key_checks=0");
+        $databaseName = DB::getDatabaseName();
+        $tables = DB::select("SELECT * FROM information_schema.tables WHERE table_schema = '$databaseName'");
+        foreach ($tables as $table) {
+            $name = $table->TABLE_NAME;
+            //if you don't want to truncate migrations
+            if ($name == 'migrations') {
+                continue;
+            }
+            DB::table($name)->truncate();
+        }
+        DB::statement("SET foreign_key_checks=1");
 
         $this->call([
             PermissionSeeder::class,
@@ -42,6 +42,6 @@ class DatabaseSeeder extends Seeder
             MovieSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
-        Schema::enableForeignKeyConstraints()
+        Schema::enableForeignKeyConstraints();
     }
 }
