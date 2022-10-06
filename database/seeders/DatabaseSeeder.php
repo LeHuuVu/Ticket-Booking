5a6c9afda3fab6cc351f044433b11ca4dda7f76f
LeHuuVu <89371606+LeHuuVu@users.noreply.github.com>
2022-10-06 09:07:46 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,16 +17,16 @@ class DatabaseSeeder extends Seeder
         Schema::disableForeignKeyConstraints()
         
         //DB::statement("SET foreign_key_checks=0");
-        $databaseName = DB::getDatabaseName();
-        $tables = DB::select("SELECT * FROM information_schema.tables WHERE table_schema = '$databaseName'");
-        foreach ($tables as $table) {
-            $name = $table->TABLE_NAME;
-            //if you don't want to truncate migrations
-            if ($name == 'migrations') {
-                continue;
-            }
-            DB::table($name)->truncate();
-        }
+//         $databaseName = DB::getDatabaseName();
+//         $tables = DB::select("SELECT * FROM information_schema.tables WHERE table_schema = '$databaseName'");
+//         foreach ($tables as $table) {
+//             $name = $table->TABLE_NAME;
+//             //if you don't want to truncate migrations
+//             if ($name == 'migrations') {
+//                 continue;
+//             }
+//             DB::table($name)->truncate();
+//         }
         //DB::statement("SET foreign_key_checks=1");
 
         $this->call([
