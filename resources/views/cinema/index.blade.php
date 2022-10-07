bcd7d90297547ea4d04eca791cb145895277919e
NguyenMinhQuan <85224925+minhquanhbt@users.noreply.github.com>
2022-10-07 08:58:33 +0700

    Update index.blade.php

--- resources/views/cinema/index.blade.php
+++ resources/views/cinema/index.blade.php
@@ -149,8 +149,7 @@
                         let dateCmp = new Date(data.movies[key].schedules[x].start_at)
                         if ((dateCmp.getDate()) == dates[index].getDate()){
                             let t = document.createElement('a');
-                            if({{Auth::check()? 'true' : 'false'}} == 'true')
-                            {
+                            @auth
                                 var role_id = "{{Auth::user()->role_id}}"
                                 if(role_id == {{\App\Models\User::ROLE_CUSTOMER}})
                                 {
@@ -160,7 +159,7 @@
                                 {
                                     t.setAttribute('href',"/schedule/"+data.movies[key].schedules[x].id+"/edit")
                                 }
-                            }
+                            @endauth
                             
                             t.setAttribute('class','btn');
                             t.innerHTML = data.movies[key].schedules[x].play_time;
